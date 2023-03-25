50dca869cfbbd65eb515b1a91d9891d8afff1975
Xavius123 <dantweigel@gmail.com>
2023-03-25 11:00:04 -0500

    basic setup Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />ß
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -14,8 +14,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
