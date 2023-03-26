7962d3ffadf89bba40cc970e7bee669ad67fa9fc
Xavius123 <dantweigel@gmail.com>
2023-03-26 01:43:45 -0500

    added scss and stepper

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
 import { alphabetArray } from './helpers/string.helper';
 
 function App() {
+    const [step, setStep] = useState(5);
     const alphabetLowerCase = alphabetArray(false);
     const alphabetUpperCase = alphabetArray(true);
 
-    console.log(alphabetLowerCase);
-    console.log(alphabetUpperCase);
+    // console.log(alphabetLowerCase);
+    // console.log(alphabetUpperCase);
+
+    const onChange = (event: any) => {
+        const currentStep = event.target.value;
+        setStep(currentStep);
+    };
+
     return (
         <div className="App">
             <h1>Password Generator</h1>
+            <input
+                type="range"
+                className="slider"
+                value={step}
+                min="0"
+                max="100"
+                onChange={(e) => onChange(e)}
+            />
         </div>
     );
 }
